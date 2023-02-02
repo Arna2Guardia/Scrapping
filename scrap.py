3149b04dbad1f7087afec608f38880bc639001a0
Arna2Guardia <117608420+Arna2Guardia@users.noreply.github.com>
2023-02-02 10:35:50 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -3,7 +3,7 @@ import bs4
 import time
 
 baseUrl = 'https://stockx.com'
-uri = '/fr-fr/sneakers'
+uri = '/fr-fr/sneakers?page='
 
 
 headers = {
@@ -21,6 +21,11 @@ response = requests.get(baseUrl + uri, headers=headers)
 print(response) 
 
 if response.ok:
+    webpoints = []
+    for i in range(25):
+        webpoints.append(baseUrl + uri + str(i))
+    print(webpoints)
+    endpoints = []
     swoup = bs4.BeautifulSoup(response.text,'html.parser')
     #print(swoup.prettify())
     id = swoup.find('div', {"id": "browse-grid"})
@@ -31,25 +36,28 @@ if response.ok:
       #print(a)
 
       try:
+        price = price.contents[0]
         print("Link:", baseUrl + a['href'])
-        print("Price:", price.contents[0])
+        print("Price:", price)
+        endpoints.append([baseUrl + a['href'], price.replace(u'\xa0', ' ')])
+        print(endpoints)
         new = requests.get(baseUrl + a['href'], headers=headers)
         #print(new)
 
         if new.ok:
           swoup2 = bs4.BeautifulSoup(new.text,'html.parser')
           #print(swoup2.prettify())
-          bdm = swoup2.find('button', attrs= {"id": 'menu-button-pdp-size-selector'})
-          bdm['aria-expanded'] = "true"
-          print(bdm)
+          #bdm = swoup2.find('button', attrs= {"id": 'menu-button-pdp-size-selector'})
+          #bdm['aria-expanded'] = "true"
+          #print(bdm)
           ddm = swoup2.find('div', attrs= {"class": 'css-r6z5ec'})
-          ddm['style'] = "visibility: visible; min-width: max-content; --popper-transform-origin: top left; position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(1048px, 284px); width: 428px;"
-          print(ddm)
+          #ddm['style'] = "visibility: visible; min-width: max-content; --popper-transform-origin: top left; position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(1048px, 284px); width: 428px;"
+          #print(ddm)
           grid_price = swoup2.find('div', attrs= {"id": 'menu-list-pdp-size-selector'})
-          grid_price['style'] = "transform-origin: var(--popper-transform-origin); opacity: 1; visibility: visible; transform: none;"
-          print(grid_price)
+          #grid_price['style'] = "transform-origin: var(--popper-transform-origin); opacity: 1; visibility: visible; transform: none;"
+          #print(grid_price)
           prices_size = swoup2.findAll('button', {"class": "css-10ur78r"})
-          print(prices_size)
+          #print(prices_size)
 
           for but in prices_size:
             div_price = but.find('div', {"class": "css-1pulpde"})
@@ -59,7 +67,7 @@ if response.ok:
               print('Price for:',pointure,' is:',div_price)
 
             except:
-              print('ça marche pas fdp')
+              print('ça marche pas')
               pass
 
       except:
