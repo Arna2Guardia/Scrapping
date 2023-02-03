55cd06c8598dc512843048444fb48bd80a867569
Arna2Guardia <117608420+Arna2Guardia@users.noreply.github.com>
2023-02-03 01:50:43 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -5,7 +5,6 @@ import time
 baseUrl = 'https://stockx.com'
 uri = '/fr-fr/sneakers?page='
 
-
 headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
   "Accept-Language": "en-US,en;q=0.8",
@@ -22,56 +21,93 @@ print(response)
 
 if response.ok:
     webpoints = []
-    for i in range(25):
+
+    for i in range(1,25):
         webpoints.append(baseUrl + uri + str(i))
-    print(webpoints)
+    #print(webpoints)  
+
     endpoints = []
-    swoup = bs4.BeautifulSoup(response.text,'html.parser')
-    #print(swoup.prettify())
-    id = swoup.find('div', {"id": "browse-grid"})
-    divs = swoup.findAll('div', {"class": "css-1ibvugw-GridProductTileContainer"})
-    for div in divs:
-      a = div.find('a')
-      price = div.find('p',{"class": "css-9ryi0c"})
-      #print(a)
-
-      try:
-        price = price.contents[0]
-        print("Link:", baseUrl + a['href'])
-        print("Price:", price)
-        endpoints.append([baseUrl + a['href'], price.replace(u'\xa0', ' ')])
-        print(endpoints)
-        new = requests.get(baseUrl + a['href'], headers=headers)
-        #print(new)
-
-        if new.ok:
-          swoup2 = bs4.BeautifulSoup(new.text,'html.parser')
-          #print(swoup2.prettify())
-          #bdm = swoup2.find('button', attrs= {"id": 'menu-button-pdp-size-selector'})
-          #bdm['aria-expanded'] = "true"
-          #print(bdm)
-          ddm = swoup2.find('div', attrs= {"class": 'css-r6z5ec'})
-          #ddm['style'] = "visibility: visible; min-width: max-content; --popper-transform-origin: top left; position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(1048px, 284px); width: 428px;"
-          #print(ddm)
-          grid_price = swoup2.find('div', attrs= {"id": 'menu-list-pdp-size-selector'})
-          #grid_price['style'] = "transform-origin: var(--popper-transform-origin); opacity: 1; visibility: visible; transform: none;"
-          #print(grid_price)
-          prices_size = swoup2.findAll('button', {"class": "css-10ur78r"})
-          #print(prices_size)
-
-          for but in prices_size:
-            div_price = but.find('div', {"class": "css-1pulpde"})
-            pointure = but.find('div', {"class": "css-nszg6y"})
+    for webpoint in webpoints:
+        response = requests.get(webpoint, headers=headers)
+        if response.ok:
+          swoup = bs4.BeautifulSoup(response.text,'html.parser')
+          #print(swoup.prettify())
+          id = swoup.find('div', {"id": "browse-grid"})
+          divs = swoup.findAll('div', {"class": "css-1ibvugw-GridProductTileContainer"})
+          for div in divs:
+            a = div.find('a')
+            price = div.find('p',{"class": "css-9ryi0c"})
+            #print(a)
+            #price = price.contents[0]
+            # int_price = int(price.replace('\xa0€',''))
+            # print("LE PRIX EST: \n", int_price)
 
             try:
-              print('Price for:',pointure,' is:',div_price)
+              price = price.contents[0]
+              intPrice = int(price.replace('\xa0€',''))
+              print("Link:", baseUrl + a['href'])
+              print("Price:", price)
+              endpoints.append([baseUrl + a['href'], price.replace(u'\xa0', ' ')])
+              #print(endpoints)
+              new = requests.get(baseUrl + a['href'], headers=headers)
+              #print(new)
+
+              if new.ok:
+                swoup2 = bs4.BeautifulSoup(new.text,'html.parser')
+                lastSellAll = swoup2.find('p', {'class': 'css-xfmxd4'})
+                lastSellAll = lastSellAll.contents[0]
+                intLastSellAll = int(lastSellAll.replace('\xa0€',''))                
+                #print('INTLASTSELLALL IS \n:', intLastSellAll)
+                res = intPrice - intLastSellAll
+                #print(res)
+
+                try:
+                  difference = swoup2.findAll('p', {'class': 'css-eqh2n0'})
+                  print('Last sell:', lastSellAll, '\n')
+                  cpt = 0
+                  for dif in difference:
+                    if cpt == 0:
+                      print('Difference between 2 last sales:', dif.contents[0], '\n')
+                    else:
+                      print(dif.contents[0], '\n')
+                    cpt +=1
+                except:
+                  print("Class css-eqh2n0 not found")
+                  pass
+
+                try:
+                  difference = swoup2.findAll('p', {'class': 'css-as46lx'})
+                  #print('Last sell:', lastSellAll, '\n')
+                  cpt = 0
+                  for dif in difference:
+                    if cpt == 0:
+                      print('Difference between 2 last sales:', dif.contents[0], '\n')
+                    else:
+                      print(dif.contents[0], '\n')
+                    cpt +=1
+                except:
+                  print('Class css-as46lx not found')
+                  pass
+              
+                try:
+                  difference = swoup2.findAll('p', {'class': 'css-1qumzfe'})
+                  #print('Last sell:', lastSellAll, '\n')
+                  cpt = 0
+                  for dif in difference:
+                    if cpt == 0:
+                      print('Difference between 2 last sales:', dif.contents[0], '\n')
+                    else:
+                      print('+',dif.contents[0], '\n')
+                    cpt +=1
+                except:
+                  print('Class css-1qumzfe not found')
+                  pass
+              
+
+
 
             except:
-              print('ça marche pas')
               pass
 
-      except:
-        pass
-
-      time.sleep(5)
+            time.sleep(1)
     #print('\nL id est:', id)
